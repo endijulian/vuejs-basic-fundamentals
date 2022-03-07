b102717124c9f90c612ca66f50da1abed2aaaf70
endi julian <endi@iglobal.co.id>
2022-03-07 21:11:13 +0700

    css scoped

---
+++ basic/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar></Navbar>
+    <nav>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Post</a></li>
+        <li><a href="">About</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+  import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      title: 'How Are You Endi Julian'
+    }
+  },
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
